5e459188c6e79fa6d84a1ada16c1951971625451
M. Nathaniel Martin <matt.martin@rutgers.edu>
2016-03-16 19:45:28 -0400

    openweather API added!

--- scripts.js
+++ scripts.js
@@ -30,30 +30,23 @@ $(document).ready(function(){
 			console.log('Longitude: ' + longitude);
 
 			//will need more AJAX calls to fill other APIs with this info.
-			var googlePlaceQueryURL ='https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + location.trim() + '&key=AIzaSyDWy1HdPMsWJKHz-ZCCZ7nOugsFJFvVxm0'
 
-			$.ajax({url: googlePlaceQueryURL, method: 'GET'})
-			.done(function(response) {
+			// openWeather API Key = b0b52307eaa0d845eca3022f719aae3d
+			var openWeatherURL = 'http://api.openweathermap.org/data/2.5/find?lat=' + latitude + '&lon=' + longitude + '&cnt=1&APPID=b0b52307eaa0d845eca3022f719aae3d';
 
-				var data = response.results[0];
+
+				$.ajax({url: openWeatherURL, method: 'GET'})
+				.done(function(response) {
+
 				// results[0] pulls the FIRST result from geocoder API.
 				
+		 		console.log("Open Weather!");
 		 		console.log(response);
-		 	// 	var location = data.formatted_address;
-				// console.log('Location Query: ' + location);
 
-				// var place_id = data.place_id;
-				// console.log('GooglePlace ID: ' + place_id);
 
-				// var latitude = data.geometry.location.lat;
-				// console.log('Latitude: ' + latitude);
+			});
 
-				// var longitude = data.geometry.location.lng;
-				// console.log('Longitude: ' + longitude);
-
-				//will need more AJAX calls to fill other APIs with this info.
-
-			}); 
+			
 
 		}); 
 	}
