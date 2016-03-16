8705a221fdf535856cb5d1b55278deaeccec8046
Matthew N. Martin <matt@angryjenkins.com>
2016-03-16 17:42:42 -0400

    last push from work, Places API error, server related?

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
 
 // sample geocoder API URL : https://maps.googleapis.com/maps/api/geocode/json?address=London&key=AIzaSyAzBECPmc6z_ppq-pud2BgfA6bmZOnC25s
 
+// sample google Places URL https://maps.googleapis.com/maps/api/place/textsearch/json?parameters
+
 //geocoder function with AJAX calls.
 $(document).ready(function(){
 	var geocoder = function (){
 	var query = $('#search').val().trim();
-	var geocodeQueryURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + query + '&key=AIzaSyAzBECPmc6z_ppq-pud2BgfA6bmZOnC25s'
+	var geocodeQueryURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + query + '&key=AIzaSyAzBECPmc6z_ppq-pud2BgfA6bmZOnC25s';
+	
 
 		$.ajax({url: geocodeQueryURL, method: 'GET'})
 			.done(function(response) {
@@ -27,6 +30,30 @@ $(document).ready(function(){
 			console.log('Longitude: ' + longitude);
 
 			//will need more AJAX calls to fill other APIs with this info.
+			var googlePlaceQueryURL ='https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + location.trim() + '&key=AIzaSyDWy1HdPMsWJKHz-ZCCZ7nOugsFJFvVxm0'
+
+			$.ajax({url: googlePlaceQueryURL, method: 'GET'})
+			.done(function(response) {
+
+				var data = response.results[0];
+				// results[0] pulls the FIRST result from geocoder API.
+				
+		 		console.log(response);
+		 	// 	var location = data.formatted_address;
+				// console.log('Location Query: ' + location);
+
+				// var place_id = data.place_id;
+				// console.log('GooglePlace ID: ' + place_id);
+
+				// var latitude = data.geometry.location.lat;
+				// console.log('Latitude: ' + latitude);
+
+				// var longitude = data.geometry.location.lng;
+				// console.log('Longitude: ' + longitude);
+
+				//will need more AJAX calls to fill other APIs with this info.
+
+			}); 
 
 		}); 
 	}
