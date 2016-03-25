b8fc2eeceede8d5e75cea3e8d68162a29ca80bee
@angryjenkins <matt@angryjenkins.com>
2016-03-24 23:35:04 -0400

    push from desktop - styles on buzz, 429 on weather ="

--- scripts.js
+++ scripts.js
@@ -43,7 +43,7 @@ $(document).ready(function(){
 			// $('.panelTitle').append(location);
 
 			headLoc.append(location);
-			showLocation.append('<div class="row"><div class="col-md-12 text-center"><p>' + location + '</p></div><div class="col-md-12 text-center"><p>(' + latitude.toFixed(2) + ' , ' + longitude.toFixed(2) + ')</p><div></div>');
+			showLocation.append('<div class="row"><div class="col-md-12"><h2>' + location + ' <span class="text-smaller">('  + latitude.toFixed(2) + ' , ' + longitude.toFixed(2) + ')</span></h2></div></div>');
 			// showLocation.append(mapDisplay);
 
 			// attempt at google map places library
@@ -88,7 +88,7 @@ $(document).ready(function(){
 					      // placeDisplay.append('<p>Google Rating: ' + place.rating);
 
 
-				     	placeDisplay.append('<div class="media"><div class="media-left media-middle"><img class="media-object" src="' + placePhoto + '" alt="' + location + '"></div><div class="media-body"><h4 class="media-heading">' + place.name + '</h4><p>Google Rating: ' + place.rating + '<br />' + place.formatted_address + '</p><p class="text-muted">' + types  + '</p></div></div>');
+				     	placeDisplay.append('<div class="media"><div class="media-left media-middle"><img class="media-object placePic" src="' + placePhoto + '" alt="' + location + '"></div><div class="media-body"><h4 class="media-heading">' + place.name + '</h4><p>Google Rating: ' + place.rating + '<br />' + place.formatted_address + '</p><p class="text-muted">' + types  + '</p></div></div>');
 
 					      //GOTTAFIX: pull the photo from the getUrl function - put all this in a media object?
 
@@ -123,6 +123,7 @@ $(document).ready(function(){
 	 		var lowTempK = weatherData.main.temp_min;
 	 		var condition = weatherData.weather[0].description;
 	 		var windSpeed = weatherData.wind.speed;
+	 		var windSpeedMPH = windspeed * 0.62137;
 	 		var windDir = weatherData.wind.deg;
 
 
@@ -138,53 +139,32 @@ $(document).ready(function(){
 
 	 		console.log(tempF + "F , " + tempC + "C");
 
-	 		var showWeather = $('#weatherDisplay');
-			showWeather.empty();
-
-			var tempInfo = $('<div class="text-center">');
-			showLocation.append('<h2>Current Temp: '+ tempF + ' &#8457; , ' + tempC + ' &#8451;</h2>');
-			showLocation.append('<p class="alert alert-info">' + condition);
-			showLocation.append('<p>High: ' + highTempF + ' &#8457; , ' + highTempC + ' &#8451; - Low: '  + lowTempF + ' &#8457; , ' + lowTempC + ' &#8451;</p>');
-			showLocation.append('<p>Wind Speed: ' + windSpeed + 'km/h - Direction: ' + windDir + '&deg;</p>');
+			// showLocation.append('<h2>Current Temp: '+ tempF + ' &#8457; , ' + tempC + ' &#8451;</h2>');
+			showLocation.append('<p><span class="label label-info">' + condition + '</span>&nbsp;&nbsp;High: ' + highTempF + ' &#8457; , ' + highTempC + ' &#8451; - Low: '  + lowTempF + ' &#8457; , ' + lowTempC + ' &#8451;</p>');
+			showLocation.append('<p>Wind Speed: ' + windSpeed + 'km/h <span class="text-smaller">(' + windSpeedMPH + 'mph) - Direction: ' + windDir + '&deg;</p>');
 			showLocation.append('<div class="clearfix">');
 
-			showWeather.append(tempInfo);
 		});
 
-		var fiveDayForecastURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&cnt=1&APPID=b0b52307eaa0d845eca3022f719aae3d';
+		// var fiveDayForecastURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&cnt=1&APPID=b0b52307eaa0d845eca3022f719aae3d';
 
-			$.ajax({url: fiveDayForecastURL, method: 'GET'})
-			.done(function(response) {
+		// 	$.ajax({url: fiveDayForecastURL, method: 'GET'})
+		// 	.done(function(response) {
 			
-	 		console.log("------Five Day Forecast!!");
-	 		console.log(response);
-		});
+	 // 		console.log("------Five Day Forecast!!");
+	 // 		console.log(response);
+		// });
 
 		var photoQueryURL ='https://api.flickr.com/services/rest/?&method=flickr.photos.search&lat=' + latitude + '&lon=' + longitude +'&has_geo=1&per_page=5&format=json&nojsoncallback=1&api_key=883c01db966eed32014011db7cb741de';
 
 			$.ajax({url: photoQueryURL, method: 'GET'})
 				.done(function(response) {
 				var dataPhoto = response.results;
-			console.log('------Flickr Photos!');
-			console.log(response);
-		}); 
-		//not yet working - UN Data AJAX call.
-		// var UNDataQueryURL ='https://api.undata-api.org/WHO/WHO%20Data/' + country + '/records?app_id=04296eeb&app_key=043ebf5a60b2ee49dada51cb8ef705fc';
 
-		// 	$.ajax({url: UNDataQueryURL, method: 'GET'})
-		// 		.done(function(response) {
-		// 	console.log('UN Data!');
-		// 	console.log(response);
-		// }); 
-
-		//Google Places Error XMLHttpRequest cannot load - does Faisal have the fix?
-		// var googlePlacesQuery ='https://maps.googleapis.com/maps/api/place/textsearch/json?query=tourist+attractions+in+' + query + '&key=AIzaSyAzBECPmc6z_ppq';
-
-		// 	$.ajax({url: googlePlacesQuery, method: 'GET'})
-		// 		.done(function(response) {
-		// 		console.log('Google Places - "Restaurants In" Query!');
-		// 		console.log(response);
-		// 	}); 
+				console.log('------Flickr Photos!');
+				console.log(response);
+			}); 
+		
 		}); 
 	}
 
